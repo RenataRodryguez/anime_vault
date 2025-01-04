5738ba7149a505cb1acc3736c783f33c638d19fa
Renata <renata.rodriguesdasilva@outlook.com>
2025-01-04 15:36:55 -0300

    feat: Data change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Image from "next/image";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+    <footer className="sticky bottom-0 sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+      <p className="text-base font-bold text-white">@{currentYear} Renata </p>
       <Image
         src="./logo.svg"
         alt="logo"
